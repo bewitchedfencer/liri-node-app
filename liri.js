28384c3249540a6ee1d8e5c16cd61dab3bb33297
Mac User <alexarobinson19@gmail.com>
2018-01-12 15:28:47 -0500

    I managed to get the tweets printing out. Now I am trying to search spotify for a song, but it keeps returning an array of objects with nothing else in it.

--- liri.js
+++ liri.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const keys = require("./keys.js");	
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
-// var spotify = new Spotify(keys.spotify);
+
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+
+
 
 //Twitter function
 
@@ -11,14 +16,26 @@ function getTweets(){
     var params = {screen_name: 'codingMaven19'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
-        console.log(tweets.statuses.text);
-        for(var i; i<tweets.length; i++){
-            console.log(tweets.text);
-        }
+        tweets.forEach(function (element) {
+            console.log(`Created On: ${element.created_at} \n${element.text}`);
+       });
         }
     
     });
 };
 
 
-    getTweets();
+    // getTweets(); for testing the Tweet function
+
+    function getTrack(){
+        spotify
+        .search({ type: 'track', query: 'All the Small Things' })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
+
+    getTrack();
