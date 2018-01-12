6e2ca4decf3917894d402e9a7dba77a3f854f423
Mac User <alexarobinson19@gmail.com>
2018-01-12 00:58:12 -0500

    tried to solve the twitter issue, but I cannot figure out why the text of the tweets is not printing. I may need to reach out for some help on this aspect and start on the next part of the code: the function for spotify. Once I have the functions for each of the commands, it will just be a matter of writing out all the code that I need.

--- liri.js
+++ liri.js
@@ -4,17 +4,21 @@ var Twitter = require('twitter');
 
 // var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-var params = {screen_name: 'codingMaven19'};
 
 //Twitter function
 
 function getTweets(){
-    client.get('statuses/lookup', params, function(error, tweets, response) {
-        if(!error){
-        console.log(tweets);  // The favorites. 
-        console.log(response);  // Raw response object. 
+    var params = {screen_name: 'codingMaven19'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets.statuses.text);
+        for(var i; i<tweets.length; i++){
+            console.log(tweets.text);
         }
+        }
+    
     });
-    };
+};
+
 
     getTweets();
